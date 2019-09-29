d597708ef47d71bf6e1e32c3c72ce1258b0bc1e4
icedCoughfee <brentmelton99@yahoo.com>
2019-09-28 22:05:56 -0700

    initialize basic non funcitonal navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import Champions from "./components/champions";
+import Navbar from "./components/common/navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <Champions />
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <div className="container-fluid">
+          <Champions />
+        </div>
+      </React.Fragment>
     );
   }
 }
