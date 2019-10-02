1cd67c6d6dcc1091a2170f1180bc62a003c4c184
icedCoughfee <brentmelton99@yahoo.com>
2019-10-01 17:42:22 -0700

    champion navbar with active classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import React, { Component } from "react";
 import Champions from "./components/champions";
 import ChampionProfile from "./components/championProfile";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Navbar from "./components/common/navbar";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    navItems: [
+      { name: "Champions", path: "/champions" },
+      { name: "Summoners", path: "/summoners" }
+    ]
+  };
   render() {
     return (
       <React.Fragment>
         <BrowserRouter>
+          <Navbar navItems={this.state.navItems} />
           <div className="container-fluid">
             <Switch>
               <Route
---
+++ src/components/common/navbar.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    const { navItems, location } = this.props;
+    console.log(location.pathname);
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li
+              className={`nav-item ${this.navItemActive(
+                "/",
+                location.pathname
+              )}`}
+            >
+              <Link to={"/"} className="nav-link">
+                Home <span className="sr-only">(current)</span>
+              </Link>
+            </li>
+            {navItems.map(item => (
+              <li
+                key={item.name}
+                className={`nav-item ${this.navItemActive(
+                  item,
+                  location.pathname
+                )}`}
+              >
+                <Link to={item.path} className="nav-link">
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+
+  navItemActive = (item, appLocation) =>
+    item.path === appLocation ? "active" : "";
+}
+
+export default withRouter(Navbar);
