0f68cb1e672265972471979f75a20c998e557d77
icedCoughfee <brentmelton99@yahoo.com>
2019-10-13 15:28:52 -0700

    update http to https for calls to ddragon

--- src/services/championService.js
+++ src/services/championService.js
@@ -1,5 +1,7 @@
 import http from "./httpService";
 
 export function getChampions() {
-    return http.get("http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json");
+  return http.get(
+    "https://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json"
+  );
 }
